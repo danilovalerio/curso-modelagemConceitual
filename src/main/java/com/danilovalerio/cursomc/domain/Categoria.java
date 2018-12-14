76157f7e7f92165345074a0fd45e77e0bae22402
Danilo Valério da Silva <danilo_vs@hotmail.com>
2018-12-14 21:47:35 -0200

    Add Banco de dados H2, criação da base de dados e uso do JPA

--- src/main/java/com/danilovalerio/cursomc/domain/Categoria.java
+++ src/main/java/com/danilovalerio/cursomc/domain/Categoria.java
@@ -2,10 +2,18 @@ package com.danilovalerio.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity //indica que será uma entidade
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY) //estratégia de geração do ID
 	private Integer id;
 	private String nome;
 	
